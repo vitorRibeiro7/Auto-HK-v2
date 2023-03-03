66960619b69ee5b7f5c5173909b274109e5cbe94
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-03-03 09:55:27 -0300

    :rocket: loding added to listing

--- client/src/components/List/index.tsx
+++ client/src/components/List/index.tsx
@@ -9,6 +9,8 @@ import { Grid, Typography, Box, Divider } from "@mui/material";
 
 import { Cards } from "./Cards";
 
+import { LinearProgress } from "@mui/material";
+
 export const List = () => {
   const [vehicleOject, setVehicleObject] = useState<Vehicle[]>([]);
   const [currentSearch, setCurrentSearch] = useState("");
@@ -54,29 +56,40 @@ export const List = () => {
           sx={{
             display: "flex",
             flexDirection: "column",
+            height: vehicleOject.length > 0 ? "auto" : "300px",
+            alignItems: vehicleOject.length > 0 ? "none" : "center",
+            justifyContent: vehicleOject.length > 0 ? "none" : "center",
           }}
         >
-          <Typography
-            variant="overline"
-            sx={{
-              marginBottom: "15px",
-              fontFamily: "Plus Jakarta Sans",
-              fontWeight: 300,
-            }}
-          >
-            Vehicles found: {vehicleOject.length}{" "}
-          </Typography>
-          <Grid
-            container
-            rowSpacing={3}
-            columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 2 }}
-          >
-            {vehicleOject.map((vehicle, index) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                <Cards key={vehicle.id} vehicle={vehicle} />
+          {vehicleOject.length > 0 ? (
+            <>
+              <Typography
+                variant="overline"
+                sx={{
+                  marginBottom: "15px",
+                  fontFamily: "Plus Jakarta Sans",
+                  fontWeight: 300,
+                }}
+              >
+                Vehicles found: {vehicleOject.length}{" "}
+              </Typography>
+              <Grid
+                container
+                rowSpacing={3}
+                columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 2 }}
+              >
+                {vehicleOject.map((vehicle, index) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                    <Cards key={vehicle.id} vehicle={vehicle} />
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
+            </>
+          ) : (
+            <Box sx={{ width: "70%" }}>
+              <LinearProgress />
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
