cf7b3c9f90fb560bf368e77bd5beffea5fee54e7
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2023-03-10 09:31:02 -0300

    :rocket: port on console now

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,15 @@ import cors from "@fastify/cors"
 import Fastify from "fastify";
 import { appRoutes } from "./routes";
 
+const PORT = 3334
+
 const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-    port: 3334,
+    port: PORT,
 }).then(() => {
-    console.log("Servidor rodando.")
+    console.log(`Servidor rodando [${PORT}]`)
 })
